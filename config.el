493c3316b410fea05ad9c8efc5f43c1b75388831
lemos <xfe1235@gmail.com>
2021-06-28 00:40:24 +0800

    update

--- config.el
+++ config.el
@@ -19,12 +19,14 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Monaco" :size 14))
+(setq doom-font (font-spec :family "monaco" :size 14 )
+      doom-variable-pitch-font (font-spec :family "SF Pro Text"))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
+;;(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'doom-opera-light)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -54,6 +56,5 @@
 
 (setq confirm-kill-emacs nil)
 
-(set-frame-parameter (selected-frame) 'alpha '(95 . 93))
-
-(add-to-list 'default-frame-alist '(alpha . (95 . 93)))
+;;(after! org
+;;  (setq org-superstar-headline-bullets-list '("⠪")))
--- init.el
+++ init.el
@@ -58,7 +58,7 @@
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
-       ;;multiple-cursors  ; editing in many places at once
+       multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
@@ -147,7 +147,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +pretty +brain)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
